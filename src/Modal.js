e70679af85450b28f87322926fa2e78fb95523b6
osemenets21 <oleg.min.vin@gmail.com>
2023-10-02 20:41:00 +0200

    commit

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Modal = ({open, setOpen}) => {
   return (
-    
+
     // Open modal with animation
 
     <div className={`overlay animated ${open ? "show" : ""}`}>
@@ -16,7 +16,7 @@ export const Modal = ({open, setOpen}) => {
           <title />
           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
         </svg>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <img  src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="A red apple on a wooden table"/>
       </div>
     </div>
   );
